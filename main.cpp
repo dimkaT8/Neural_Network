ccc45c9e4f2ba1f7a3500949660fc324dfea4a14
dimkaT8 <dimka8@mail.ru>
2023-02-22 16:21:22 +0300

    add properties.json

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "/home/dimka/eigen-master/Eigen/Core"
+
+typedef Eigen::MatrixXd Matrix;
+typedef Eigen::VectorXd Vector;
 
  using namespace std;
  int main()
  {
+   Matrix x = Matrix::Random(400, 100);
    cout << " Hello GitHub " << endl ;
    return 0;
  }
