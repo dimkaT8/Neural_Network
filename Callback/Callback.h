6a65bdd3069f8df4dc5915e3fa2c153dbcefcb84
dimkaT8 <dimka8@mail.ru>
2023-02-25 11:19:45 +0300

    abstract base class Callback

--- Callback/Callback.h
+++ Callback/Callback.h
@@ -3,9 +3,10 @@
 #include "/home/dimka/Eigen/Core"
 #include "InitScalar.h"
 
+// abstract base class Callback
 namespace  NNE
 {
-    
+
 class Network;
 class Callback
 {
@@ -19,15 +20,9 @@ public:
         int _nepoch;   // Общее количество эпох (один прогон на всем наборе данных) в процессе обучения
         int _epoch_id; // Индекс текущей эпохи (0, 1, ..., m_nepoch-1)
 
-        Callback() :
-            _nbatch(0), _batch_id(0), _nepoch(0), _epoch_id(0)
-        {}
+        Callback() : _nbatch(0), _batch_id(0), _nepoch(0), _epoch_id(0) {}
 
         virtual ~Callback() {}
-
-        // Перед тренировкой мини-партии
-        virtual void pre_training_batch(const Network* net, const Matrix& x,const Matrix& y) {}
-        virtual void pre_training_batch(const Network* net, const Matrix& x,const IntegerVector& y) {}
 
         // После обучения мини-партии
         virtual void post_training_batch(const Network* net, const Matrix& x,const Matrix& y) {}
